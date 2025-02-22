7436b918f8f37d4b933f51cd74f3b69b8ef17f51
youngdexter <32617228+youngdexter@users.noreply.github.com>
2025-02-22 22:16:57 +0800

    Add files via upload

--- cp363背包问题的方案总数-货币系统1.cpp
+++ cp363背包问题的方案总数-货币系统1.cpp
@@ -1,4 +1,4 @@
-//cp363背包问题的方案总数-货币系统2
+//cp363背包问题的方案总数-货币系统1
 /*
 组成面值:10
 货币种类数:3
@@ -16,7 +16,7 @@ int a[11]; //货币面值
 
 long long f[101];  //f[j]表示面值为j的最大方案数
 
-int i, j, k; //循环变量
+int i,j,k; //循环变量
 
 int main(int argc, char **argv) {
 
@@ -31,10 +31,11 @@ int main(int argc, char **argv) {
 		cin >> a[i];
 	}
 
-	f[0] = 1;
+	f[0]=1;
 	for (i = 1; i <= n; i++)  //n种货币
-		for (j = a[i]; j <= m ; j++) //面值数
-			f[j] = f[j]+f[j - a[i]];
+		for (j = m; j >= a[i]; j--) //面值数
+			for (k = 1; k <= j/a[i]; k++) 
+				f[j]=f[j]+f[j-k*a[i]];
 
 	cout << "f[m]:" << f[m] << endl;
 	return 0;
